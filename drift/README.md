f504d9f6622aa84ef2544a0149816c5a01d29dc1
Morioh <m.onyonyi@alustudent.com>
2024-02-10 18:06:26 +0200

    Assignment 3

--- drift/README.md
+++ drift/README.md
@@ -1,4 +1,4 @@
-# drift
+# Drift
 
 A new Flutter project.
 
--- drift/android/app/src/main/kotlin/com/example/drift/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.drift
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
