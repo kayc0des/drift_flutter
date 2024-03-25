8a27c2a28bf690d024f1b76be0d5ac945c4e8244
Obedine-Flore <f.obedine@alustudent.com>
2024-03-25 14:30:39 +0100

    created test files

---
+++ drift/test/register_test.dart
@@ -0,0 +1,44 @@
+import 'package:drift/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+ testWidgets('Find registration button', (tester) async {
+   // arrange
+   await tester.pumpWidget(
+     MaterialApp(
+       home: UserPage(),
+     ),
+   );
+
+
+   final Finder signinbtn = find.byType(ElevatedButton);
+
+
+   // assert
+   expect(signinbtn, findsOneWidget);
+ });
+}
+import 'package:drift/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+
+void main() {
+ testWidgets('Find registration button', (tester) async {
+   // arrange
+   await tester.pumpWidget(
+     MaterialApp(
+       home: UserPage(),
+     ),
+   );
+
+
+   final Finder signinbtn = find.byType(ElevatedButton);
+
+
+   // assert
+   expect(signinbtn, findsOneWidget);
+ });
+}
