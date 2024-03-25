d6ef5765951f6889c67645793d455f111ee821fd
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2024-03-25 14:33:43 +0200

    widget tests updated

---
+++ drift/test/register_test.dart
@@ -0,0 +1,19 @@
+import 'package:drift/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Find registration button', (tester) async {
+    // arrange
+    await tester.pumpWidget(
+      MaterialApp(
+        home: UserPage(),
+      ),
+    );
+
+    final Finder signinbtn = find.byType(ElevatedButton);
+
+    // assert
+    expect(signinbtn, findsOneWidget);
+  });
+}
