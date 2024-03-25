30264f9bcb6b8442f2dd7a157b89385aaeafaa12
Obedine-Flore <f.obedine@alustudent.com>
2024-03-25 15:18:49 +0100

    tests added

--- drift/test/register_test.dart
+++ drift/test/register_test.dart
@@ -18,27 +18,7 @@ void main() {
 
    // assert
    expect(signinbtn, findsOneWidget);
- });
-}
-import 'package:drift/register.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-
-void main() {
- testWidgets('Find registration button', (tester) async {
-   // arrange
-   await tester.pumpWidget(
-     MaterialApp(
-       home: UserPage(),
-     ),
-   );
-
-
-   final Finder signinbtn = find.byType(ElevatedButton);
-
-
-   // assert
-   expect(signinbtn, findsOneWidget);
+   expect(find.text('Email'), findsOneWidget);
+   expect(find.text('Password'), findsOneWidget);
  });
 }
